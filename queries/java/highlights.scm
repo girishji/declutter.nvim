f5ff2d12e4244a45bed2cce4f343220c11554ce9
Girish Palya <girishji@gmail.com>
2023-04-17 20:17:55 +0200

    new queries
    A  queries/java/highlights.scm
    A  queries/python/highlights.scm

---
+++ queries/java/highlights.scm
@@ -0,0 +1,8 @@
+(interface_declaration
+  name: (identifier) @interface)
+(class_declaration
+  name: (identifier) @class)
+(record_declaration
+  name: (identifier) @record)
+(enum_declaration
+  name: (identifier) @enum)
