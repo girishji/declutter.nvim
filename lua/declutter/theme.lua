efbd75bc4339c8ab0110b621096e1e8ee05608ef
Girish Palya <girishji@gmail.com>
2023-05-03 00:57:08 +0200

    removed background colors
    M  lua/declutter/theme.lua

--- lua/declutter/theme.lua
+++ lua/declutter/theme.lua
@@ -13,58 +13,58 @@
 
 local M = {}
 
-M.colors = {
-  fg_muted_1 = 0,    -- black
-  fg_muted_2 = 8,    -- bright black
-  bg_muted_1 = 7,    -- overlay for ui elemeents
-  bg_muted_2 = 15,   -- proxy for bg, cursorline
-  primary = 5,       -- magenta
-  bg_primary = 13,   -- bright magenta
-  secondary = 6,     -- cyan
-  bg_secondary = 14, -- bringt cyan (Operator)
-  red = 1,
-  green = 2,
-  yellow = 3,
-  blue = 4,
-  bg_red = 9, -- bright red
-  bg_green = 10,
-  bg_yellow = 11,
-  bg_blue = 12,
-}
+M.get_colors = function()
+  local background = vim.o.background
+  return {
+    fg = background == 'light' and 0 or 7,
+    red = 1,
+    green = 2,
+    yellow = 3,
+    blue = 4,
+    magenta = 5,
+    cyan = 6,
+    bg = background == 'light' and 7 or 0,
+    br_fg = background == 'light' and 8 or 15,
+    br_red = 9,
+    br_green = 10,
+    br_yellow = 11,
+    br_blue = 12,
+    br_magenta = 13,
+    br_cyan = 14,
+    br_bg = background == 'light' and 15 or 8,
+  }
+end
 
 function M.setup(opts)
-  local c = M.colors
+  local c = M.get_colors()
 
   local highlights = {
     -- VIM UI
     -- https://vimdoc.sourceforge.net/htmldoc/syntax.html
-    --
     -- By not setting Normal and NormalNC we get bg and fg set by Terminal, including transparent bg.
-    -- Consequently, we cannot determine fg and bg color without querying the terminal.
-    --   Substitute 'bold cyan' as proxy for bg color
     -- Normal = for Normal mindowns, NormalNC = for non current windows
-    Cursor                   = { ctermbg = c.primary },
-    CursorLine               = { ctermbg = c.bg_muted_2, cterm = {} },
-    MatchParen               = { ctermbg = c.bg_blue },
-    ColorColumn              = { ctermbg = c.bg_muted_1 },
-    SpellBad                 = { ctermbg = c.bg_red, cterm = { underline = true } },
-    SpellCap                 = { ctermbg = c.bg_green, cterm = { underline = true } },
-    SpellRare                = { ctermbg = c.bg_yellow, cterm = { underline = true } },
-    SpellLocal               = { ctermbg = c.bg_blue, cterm = { underline = true } },
-    NonText                  = { ctermfg = c.fg_muted_2 }, -- some text in :nmap
-    LineNr                   = { ctermfg = c.fg_muted_1 },
-    CursorLineNr             = { ctermbg = c.bg_muted_2, ctermfg = c.fg_muted_2, cterm = {} },
-    Visual                   = { ctermbg = c.bg_primary },
-    IncSearch                = { ctermfg = c.primary, cterm = { reverse = true } },
-    Search                   = { ctermfg = c.primary, ctermbg = c.fg_muted_1 },
-    StatusLine               = { ctermbg = c.bg_muted_1 },
+    Cursor                   = { ctermbg = c.yellow },
+    CursorLine               = { ctermbg = c.br_bg, cterm = {} },
+    CursorLineNr             = { ctermbg = c.br_bg, ctermfg = c.br_fg, cterm = {} },
+    -- MatchParen             = { ctermbg = c.bg },
+    -- ColorColumn            = { ctermbg = c.bg },
+    SpellBad                 = { ctermfg = c.red, cterm = { underline = true } },
+    SpellCap                 = { ctermfg = c.green, cterm = { underline = true } },
+    SpellRare                = { ctermfg = c.yellow, cterm = { underline = true } },
+    SpellLocal               = { ctermfg = c.blue, cterm = { underline = true } },
+    NonText                  = { ctermfg = c.br_fg }, -- some text in :nmap
+    LineNr                   = { ctermfg = c.fg },
+    Visual                   = { ctermbg = c.bg },
+    -- IncSearch              = { ctermfg = c.primary, cterm = { reverse = true } },
+    -- Search                 = { ctermfg = c.primary, ctermbg = c.secondary_bg },
+    StatusLine               = { ctermbg = c.br_bg },
     StatusLineNC             = { ctermbg = 'NONE' },
-    TabLine                  = { ctermbg = c.bg_muted_1 },
-    TabLineSel               = { ctermbg = c.bg_muted_2 },
-    Folded                   = { ctermfg = c.primary, cterm = { bold = true } },
-    Conceal                  = { ctermfg = c.primary },
-    Directory                = { ctermfg = c.bg_blue },
-    Title                    = { ctermfg = c.primary, cterm = { bold = true } },
+    TabLine                  = { ctermbg = c.br_bg },
+    TabLineSel               = { ctermbg = c.br_bg },
+    -- Folded                 = { ctermfg = c.primary, cterm = { bold = true } },
+    -- Conceal                = { ctermfg = c.primary },
+    Directory                = { ctermfg = c.blue },
+    Title                    = { ctermfg = c.yellow, cterm = { bold = true } },
     ErrorMsg                 = { link = 'DiffDelete' }, -- error messages on the command line
     WarningMsg               = { link = 'DiffChange' },
     MoreMsg                  = { link = 'Title' },      -- 'show more' prompt
@@ -78,28 +78,28 @@ function M.setup(opts)
     SpecialKey               = { link = 'NonText' },
     --
     -- nvim -d (vimdiff)
-    DiffAdd                  = { ctermbg = c.bg_green },
-    DiffChange               = { ctermbg = c.bg_yellow },
-    DiffDelete               = { ctermbg = c.bg_red },
-    DiffText                 = { ctermbg = c.bg_primary },
+    DiffAdd                  = { ctermbg = c.br_bg, ctermfg = c.green },
+    DiffChange               = { ctermbg = c.br_bg, ctermfg = c.yellow },
+    DiffDelete               = { ctermbg = c.br_bg, ctermfg = c.red },
+    DiffText                 = { ctermbg = c.br_bg, ctermfg = c.blue, cterm = { bold = true } },
     --
     -- Floating Windows
-    FloatBorder              = { ctermfg = 'NONE', ctermbg = c.bg_muted_1 },
-    NormalFloat              = { ctermfg = 'NONE', ctermbg = c.bg_muted_1 }, -- Normal text in floating windows.
-    -- FloatTitle               = { cterm = { bold = true } },
+    FloatBorder              = { ctermfg = 'NONE', ctermbg = c.br_bg },
+    NormalFloat              = { ctermfg = 'NONE', ctermbg = c.br_bg }, -- Normal text in floating windows.
+    FloatTitle               = { cterm = { bold = true } },
     -- NormalSB            = { ctermfg = c.fg_sidebar, ctermbg = c.bg_sidebar }, -- normal text in sidebar
     --
-    WinSeparator             = { link = 'FloatBorder' },
+    -- WinSeparator             = { link = 'FloatBorder' },
     --
     Pmenu                    = { link = 'NormalFloat' },
     PmenuSel                 = { link = 'Visual' },
-    PmenuSBar                = { link = 'Visual' },        -- scrollbar
-    PmenuThumb               = { ctermbg = c.fg_muted_1 }, -- thumb of scrollbar
+    PmenuSBar                = { link = 'Visual' },   -- scrollbar
+    PmenuThumb               = { ctermbg = c.br_fg }, -- thumb of scrollbar
     --
     -- Generic syntax
     -- https://vimdoc.sourceforge.net/htmldoc/syntax.html
     --    *Comment	any comment
-    Comment                  = { ctermfg = c.fg_muted_2, cterm = { italic = true } },
+    Comment                  = { ctermfg = c.br_fg, cterm = { italic = true } },
     --
     -- *Constant	any constant
     --  String		a string constant: "this is a string"
@@ -157,10 +157,10 @@ function M.setup(opts)
     --  SpecialComment	special things inside a comment
     --  Debug		debugging statements
     Special                  = { ctermfg = 'NONE' },
-    Delimiter                = { ctermfg = c.secondary, cterm = {} }, -- operator symbols
+    Delimiter                = { ctermfg = c.yellow, cterm = {} }, -- operator symbols
     --
     -- *Underlined	text that stands out, HTML links
-    Underlined               = { ctermfg = c.fg_muted_2, cterm = { underline = true } },
+    Underlined               = { ctermfg = c.br_fg, cterm = { underline = true } },
     --
     -- *Ignore		left blank, hidden  |hl-Ignore|
     Ignore                   = { ctermfg = 'NONE' },
@@ -169,13 +169,13 @@ function M.setup(opts)
     Error                    = { link = 'ErrorMsg' },
     --
     -- *Todo		anything that needs extra attention; mostly the
-    Todo                     = { ctermfg = c.primary, ctermbg = 'NONE', cterm = { bold = true, underline = true } },
+    Todo                     = { ctermfg = c.yellow, ctermbg = 'NONE', cterm = { bold = true, underline = true } },
     --
 
     -- Set specific Treesitter node tags
     -- ["@type.qualifier"]      = { link = 'PreProc' },
-    ["@text.danger"]         = { ctermfg = c.fg_yellow, cterm = { underline = true } },
-    ["@text.uri"]            = { ctermfg = c.fg_muted_2 },
+    ["@text.danger"]         = { ctermfg = c.yellow, cterm = { underline = true } },
+    ["@text.uri"]            = { ctermfg = c.br_fg },
     ["@text.literal"]        = { link = 'NONE' },
     --
 
@@ -183,11 +183,7 @@ function M.setup(opts)
     DiagnosticError          = { link = 'Error' },
     DiagnosticWarn           = { link = 'WarningMsg' },
     DiagnosticInfo           = { link = 'DiffText' },
-    DiagnosticHint           = { ctermbg = c.bg_green },
-    DiagnosticUnderlineError = { ctermbg = 'NONE', ctermfg = 'NONE', cterm = { undercurl = true, sp = c.fg_red } },
-    DiagnosticUnderlineWarn  = { ctermbg = 'NONE', ctermfg = 'NONE', cterm = { undercurl = true, sp = c.fg_orange } },
-    DiagnosticUnderlineInfo  = { ctermbg = 'NONE', ctermfg = 'NONE', cterm = { undercurl = true, sp = c.fg_green } },
-    DiagnosticUnderlineHint  = { ctermbg = 'NONE', ctermfg = 'NONE', cterm = { undercurl = true, sp = c.fg_green } },
+    DiagnosticHint           = { ctermbg = c.br_bg, ctermfg = c.green },
     DiagnosticSignError      = { ctermbg = 'NONE', ctermfg = c.red },
     DiagnosticSignWarn       = { ctermbg = 'NONE', ctermfg = c.yellow },
     DiagnosticSignInfo       = { ctermbg = 'NONE', ctermfg = c.blue },
@@ -195,13 +191,13 @@ function M.setup(opts)
     --
 
     -- nvim-cmp
-    CmpItemAbbrMatch         = { ctermfg = c.primary },
+    CmpItemAbbrMatch         = { ctermfg = c.yellow },
     CmpItemAbbrSelect        = { link = 'Visual' },
     CmpItemAbbrMatchFuzzy    = { cterm = { bold = true } },
     --
 
     -- indent-blankline
-    IndentBlanklineIndent1   = { ctermfg = c.fg_muted_1, cterm = { nocombine = true } },
+    IndentBlanklineIndent1   = { ctermfg = c.fg, cterm = { nocombine = true } },
     IndentBlanklineIndent2   = { link = 'IndentBlanklineIndent1' },
     IndentBlanklineIndent3   = { link = 'IndentBlanklineIndent1' },
     IndentBlanklineIndent4   = { link = 'IndentBlanklineIndent1' },
@@ -213,36 +209,36 @@ function M.setup(opts)
     TelescopeNormal          = { link = 'NormalFloat' },
     TelescopeBorder          = { link = 'FloatBorder' },
     TelescopeSelection       = { link = 'Visual' },
-    TelescopeMatching        = { ctermfg = c.primary },
+    TelescopeMatching        = { ctermfg = c.yellow },
     --
     -- git
     GitSignsAdd              = { ctermbg = 'NONE', ctermfg = c.green },
     GitSignsChange           = { ctermbg = 'NONE', ctermfg = c.yellow },
     GitSignsDelete           = { ctermbg = 'NONE', ctermfg = c.red },
     -- Statusline
-    User1                    = { ctermbg = c.bg_muted_1, ctermfg = 'NONE' },
-    User2                    = { ctermbg = c.bg_primary, ctermfg = 'NONE' },
-    User3                    = { ctermbg = c.bg_muted_1, ctermfg = c.secondary },
-    User4                    = { ctermbg = c.bg_muted_1, ctermfg = c.fg_muted_1 },
-    User5                    = { ctermbg = c.bg_muted_1, ctermfg = c.red },
-    User6                    = { ctermbg = c.bg_muted_1, ctermfg = c.green },
-    User7                    = { ctermbg = c.bg_muted_1, ctermfg = c.yellow },
-    User8                    = { ctermbg = c.bg_muted_1, ctermfg = c.blue },
-    User9                    = { ctermbg = c.bg_muted_1, ctermfg = 'NONE', cterm = { italic = true } },
+    User1                    = { ctermbg = c.br_bg, ctermfg = 'NONE' },
+    User2                    = { ctermbg = c.bg, ctermfg = 'NONE' },
+    User3                    = { ctermbg = c.br_bg, ctermfg = c.magenta },
+    User4                    = { ctermbg = c.br_bg, ctermfg = c.fg },
+    User5                    = { ctermbg = c.br_bg, ctermfg = c.red },
+    User6                    = { ctermbg = c.br_bg, ctermfg = c.green },
+    User7                    = { ctermbg = c.br_bg, ctermfg = c.yellow },
+    User8                    = { ctermbg = c.br_bg, ctermfg = c.blue },
+    User9                    = { ctermbg = c.br_bg, ctermfg = 'NONE', cterm = { italic = true } },
     -- Treesitter
     ["@type.qualifier"]      = { ctermfg = 'NONE', cterm = { italic = true } },
     ["@type.builtin"]        = { link = "@type.qualifier" },
     ["@keyword.return"]      = { link = "@type.qualifier" },
     ["@variable.builtin"]    = { link = "@type.qualifier" },
     -- treesitter for markdown (treesitter.lua)
-    FencedCodeBlock          = { ctermbg = c.bg_secondary },
+    FencedCodeBlock          = { ctermbg = c.br_bg },
     BlockQuote               = { link = 'FencedCodeBlock' },
-    ["@code.inline"]         = { ctermbg = c.bg_secondary },
-    ["@deemphasize"]         = { ctermfg = c.fg_muted_1 },
+    ["@code.inline"]         = { ctermbg = c.br_bg },
+    ["@deemphasize"]         = { ctermfg = c.fg },
     ["@text.emphasis"]       = { cterm = { italic = true } },
     ["@text.strong"]         = { cterm = { bold = true } },
     ["@text.inline_link"]    = { ctermfg = c.blue },
-    ["@text.todo.checked"]   = { ctermfg = c.secondary },
+    ["@text.todo.checked"]   = { ctermfg = c.cyan },
     ["@text.todo.unchecked"] = { link = "@text.todo.checked" },
   }
 
